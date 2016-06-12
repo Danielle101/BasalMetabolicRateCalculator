78a25bf162aef3b2e18f8e5f2f3f084b1f5139b4
Danielle H <halldan4@yahoo.com>
2016-06-11 22:33:05 -0400

    added scanner and welcome prompt

--- src/BasalMetabolicRateCalculator.java
+++ src/BasalMetabolicRateCalculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //author: D.H.
 //Date 6/11/16
 //this program calculates a persons Basal Metabolic Rate based on weight, height, and age
@@ -5,7 +7,18 @@ public class BasalMetabolicRateCalculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner (System.in);
+		
+		// welcome the user to the app
+	System.out.println("Welcome to the Basal Metabolic RateCalculator!\n\nThis app will calculate your"
+			+ " bmr based on the height, age and weight that you input.\nPlease be sure to input"
+			+ " your accurate weight, age and height.\n");
+		
+		
+		
+		
+	
 	}
 
 }
