e5c274b84e285680dacb29e8fa20f3e5cb155e17
Danielle H <halldan4@yahoo.com>
2016-06-11 22:09:55 -0400

    first commit

---
+++ src/BasalMetabolicRateCalculator.java
@@ -0,0 +1,11 @@
+//author: D.H.
+//Date 6/11/16
+//this program calculates a persons Basal Metabolic Rate based on weight, height, and age
+public class BasalMetabolicRateCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
